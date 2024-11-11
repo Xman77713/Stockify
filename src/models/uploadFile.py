df89aa12ca91313a6c2fbe03f7e0e39ea59d7ee3
axelmensonge <axel.veri@gmail.com>
2024-11-11 14:51:10 +0100

    fix: nom de variable

--- src/models/uploadFile.py
+++ src/models/uploadFile.py
@@ -4,7 +4,7 @@ import os
 async def uploadFile(file):
     file_path = os.path.join("uploadDirectory", file.filename)
 
-    with open(file_path, "wb") as buffer:
-        buffer.write(await file.read())
+    with open(file_path, "wb") as directory:
+        directory.write(await file.read())
 
     return {"filename": file.filename, "message": "File successfully saved"}
