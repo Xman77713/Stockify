bd03c2bfd97ad71e92c3f791c1c69a97f8a1e6c2
axelmensonge <axel.veri@gmail.com>
2024-11-15 22:11:22 +0100

    feat: test1

--- src/models/main.py
+++ src/models/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, File, UploadFile
+# import deleteFile
+# import readFile
+# import uploadFile
+import os
 
-import deleteFile
-import readFile
-import uploadFile
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -13,22 +14,27 @@ async def uploadFileAPI(file: UploadFile = File(...)):
     Endpoint pour upload un fichier. Le fichier est enregistré dans Stockify/uploadDirectory
     """
     try:
-        return {"Info": "Success", "Function Result": await uploadFile.uploadFile(file)}
+        file_path = os.path.join("uploadDirectory", file.filename)
+
+        with open(file_path, "wb") as directory:
+            directory.write(await file.read())
+
+        return {"Info": "Success", "Function Result": {"filename": file.filename, "message": "File successfully saved"}}
 
     except Exception as e:
         return {"Info": "Fail", "Error": str(e)}
 
 
-@app.delete("/deletefile/")
-def deleteFileAPI(filename: str):
-    """
-    Endpoint pour delete un fichier par le nom
-    """
-    try:
-        return {"Info": "Success", "Function Result": deleteFile.deleteFile(filename)}
-
-    except Exception as e:
-        return {"Info": "Fail", "Error": str(e)}
+# @app.delete("/deletefile/")
+# def deleteFileAPI(filename: str):
+#     """
+#     Endpoint pour delete un fichier par le nom
+#     """
+#     try:
+#         return {"Info": "Success", "Function Result": deleteFile.deleteFile(filename)}
+#
+#     except Exception as e:
+#         return {"Info": "Fail", "Error": str(e)}
 
 
 @app.get("/files/")
@@ -37,31 +43,30 @@ def listFilesAPI():
     Endpoint pour obtenir la liste des fichiers disponibles dans uploadDirectory
     """
     try:
-        return {"Info": "Success", "Function Result": readFile.readListeFile()}
+        return {"Info": "Success", "Function Result": os.listdir("uploadDirectory")}
 
     except Exception as e:
         return {"Info": "Fail", "Error": str(e)}
 
-
-@app.get("/file/")
-def readFileByNameAPI(filename: str):
-    """
-    Endpoint pour obtenir la liste des fichiers disponibles dans uploadDirectory
-    """
-    try:
-        return {"Info": "Success", "Function Result": readFile.readFileByName(filename)}
-
-    except Exception as e:
-        return {"Info": "Fail", "Error": str(e)}
-
-
-@app.get("/downloadfile/")
-def downloadFileAPI(filename: str):
-    """
-    Endpoint pour télécharger un fichier spécifique depuis uploadDirectory.
-    """
-    try:
-        return {"Info": "Success", "Function Result": readFile.downloadFile(filename)}
-
-    except Exception as e:
-        return {"Info": "Fail", "Error": str(e)}
+# @app.get("/file/")
+# def readFileByNameAPI(filename: str):
+#     """
+#     Endpoint pour obtenir la liste des fichiers disponibles dans uploadDirectory
+#     """
+#     try:
+#         return {"Info": "Success", "Function Result": readFile.readFileByName(filename)}
+#
+#     except Exception as e:
+#         return {"Info": "Fail", "Error": str(e)}
+#
+#
+# @app.get("/downloadfile/")
+# def downloadFileAPI(filename: str):
+#     """
+#     Endpoint pour télécharger un fichier spécifique depuis uploadDirectory.
+#     """
+#     try:
+#         return {"Info": "Success", "Function Result": readFile.downloadFile(filename)}
+#
+#     except Exception as e:
+#         return {"Info": "Fail", "Error": str(e)}
