d34f2e98f6baab949476c62b8b1e08542a5d6abf
axelmensonge <axel.veri@gmail.com>
2024-11-26 22:39:51 +0100

    fix: new BDD script

--- src/models/SQL/BDD.sql
+++ src/models/SQL/BDD.sql
@@ -4,10 +4,6 @@ CREATE DATABASE stockifyDB;
 USE stockifyDB;
 
 CREATE TABLE test (
-   id INT AUTO_INCREMENT PRIMARY KEY,
-   username VARCHAR(50) NOT NULL,
-   email VARCHAR(100) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    path VARCHAR(255)
 );
-
-INSERT INTO test (username, email) VALUES ('test1', 'test1@example.com');
-INSERT INTO test (username, email) VALUES ('test2', 'test2@example.com');
